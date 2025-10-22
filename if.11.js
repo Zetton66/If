e24024d08bba0e32b64b435c5d4d99188cc95199
Muhammad Irsyad <irsyadmuhammad46@gmail.com>
2025-10-22 19:41:49 +0700

    Tugas If 10 dan 11 Telah Selesai

--- if.11.js
+++ if.11.js
@@ -1,6 +1,5 @@
 let buah = ["apel", "jeruk", "pisang", "mangga", "anggur"];
-// Tulis kode for loop di sini untuk menampilkan indeks dan nilai array
 
-for (let i = 0; i < buah.length; i++) {       // Loop menggunakan index
-  console.log("Indeks " + i + ": " + buah[i]); // Menampilkan index dan nilai
+for (let i = 0; i < buah.length; i++) {
+  console.log("Indeks " + i + ": " + buah[i]);
 }
