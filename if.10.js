b3caa30c6e7dba08e7791743b8c97bfa931792e6
Muhammad Irsyad <irsyadmuhammad46@gmail.com>
2025-11-19 21:55:11 +0700

    4 file di update

--- if.10.js
+++ if.10.js
@@ -1,10 +1,27 @@
-let a = 0, b = 1;
-console.log(a);
-console.log(b);
+let angka1 = 10;
+let angka2 = 3;
+let operator = "/";
 
-for (let i = 1; i <= 6; i++) {
-  let c = a + b;
-  console.log(c);
-  a = b;
-  b = c;
+let hasil;
+
+if (operator === "+") {
+  hasil = angka1 + angka2;
+} else if (operator === "-") {
+  hasil = angka1 - angka2;
+} else if (operator === "*") {
+  hasil = angka1 * angka2;
+} else if (operator === "/") {
+  if (angka2 !== 0) {
+    hasil = angka1 / angka2;
+  } else {
+    console.log("Error: Pembagian dengan nol tidak diperbolehkan");
+    hasil = null;
+  }
+} else {
+  console.log("Operator tidak valid");
+  hasil = null;
+}
+
+if (hasil !== null) {
+  console.log(angka1 + " " + operator + " " + angka2 + " = " + hasil.toFixed(2));
 }
