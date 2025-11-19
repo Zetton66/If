ed489ffe16affcf69b6103cbfcaa1bc92f3b98b0
Zetton66 <irsyadmuhammad46@gmail.com>
2025-11-19 20:47:18 +0700

    Update if.5.js

--- if.5.js
+++ if.5.js
@@ -1,9 +1,9 @@
 let suhu = 28;
 
 if (suhu >= 35) {
-  console.log("Sangat Panas");
+  console.log("Suhu " + suhu + "°C: Sangat Panas");
 } else if (suhu >= 25) {
-  console.log("Panas");
+  console.log("Suhu " + suhu + "°C: Panas");
 } else if (suhu >= 15) {
-  console.log("Hangat");
+  console.log("Suhu " + suhu + "°C: Hangat");
 }
