81d947ecfe8042512c28d59837d82338e641f7c5
Zetton66 <irsyadmuhammad46@gmail.com>
2025-11-19 20:45:19 +0700

    Update if.6.js

--- if.6.js
+++ if.6.js
@@ -1,9 +1,18 @@
-let totalBelanja = 500000;
+let totalBelanja = 120000;
 
 if (totalBelanja >= 500000) {
-  console.log("Diskon 20%");
+    console.log(`
+        Total belanja: Rp ${totalBelanja}
+        Diskon 20%
+        Total bayar: Rp ${totalBelanja-(totalBelanja*20/100)}`);
 } else if (totalBelanja >= 200000) {
-  console.log("Diskon 10%");
+    console.log(`
+        Total belanja: Rp ${totalBelanja}
+        Diskon 10%
+        Total bayar: Rp ${totalBelanja-(totalBelanja*10/100)}`);
 } else if (totalBelanja >= 100000) {
-  console.log("Diskon 5%");
+    console.log(`
+        Total belanja: Rp ${totalBelanja}
+        Diskon 5%
+        Total bayar: Rp ${totalBelanja-(totalBelanja*5/100)}`);
 }
